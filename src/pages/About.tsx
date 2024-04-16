636bbf64a79e7cfc4d2f6e74ec73e83416c77f82
Oyatulloh-Isroilov* <oyatullohisroilov5@gmail.com>
2024-04-16 20:26:49 +0500

    cars card

---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+export function About() {
+  return (
+    <h1 className="text-primary">Mashinalar savdosi siz bu yerda faqatgina BMW aftomabillarini harid qilishingiz mumkin. Website haqiqiy "BMW ishqibozlari uchun chiqarilgan".</h1>
+  );
+}
